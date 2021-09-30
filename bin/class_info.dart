50b727a8fcbe62db97a7e5f4971d76e84f00abbf
ByungWoo <63662808+bw-99@users.noreply.github.com>
2021-09-30 23:20:09 +0900

    .

--- bin/class_info.dart
+++ bin/class_info.dart
@@ -29,45 +29,128 @@ Future<MySqlConnection> getDB() async {
   return conn;
 }
 
-void main(List<String> arguments) async {
-  var file = "class.xlsx";
-  var log_file = "log.txt";
-  var error_file = "error.txt";
+void insertDB(table, con, log, i) async {
+  Map<String, dynamic> data = getMapData(table.rows[i]);
 
-  var bytes = File(file).readAsBytesSync();
-  var excel = Excel.decodeBytes(bytes);
-  Sheet table = excel.tables["Sheet"];
-  MySqlConnection con = await getDB();
+  var a = await con.query(
+      " insert into CLASS_INFO (CLASS_NAME, CLASS_SECTOR_1, CLASS_SECTOR_TOTAL, CREDIT, CAMPUS, PROFESSOR, CLASS_TIME, HEAD_COUNT, CLASS_NUMBER, COLLEGE_NAME, COLLEGE_MAJOR, OPEN_TIME)" +
+          " values(?,?,?,?,?,?,?,?,?,?,?,?)",
+      [
+        data["CLASS_NAME"],
+        data["CLASS_SECTOR_1"],
+        data["CLASS_SECTOR_TOTAL"],
+        data["CREDIT"],
+        data["CAMPUS"],
+        data["PROFESSOR"],
+        json.encode(data["CLASS_TIME"]),
+        data["HEAD_COUNT"],
+        data["CLASS_NUMBER"],
+        data["COLLEGE_NAME"],
+        data["COLLEGE_MAJOR"],
+        [json.encode(data["OPEN_TIME"])]
+      ]);
+  log.writeAsString("${i} / ${table.rows.length - 1}\n", mode: FileMode.append);
+}
 
-  var log = File(log_file);
-  var error = File(error_file);
-
-  log.writeAsString("FILE LOG\n");
-
+void readColdata(table) async {
+  ColData data = ColData();
   // table.rows.length - 1
   for (int i = 1; i < table.rows.length - 1; i++) {
-    print((i / table.rows.length) * 100);
-    Map<String, dynamic> data = getMapData(table.rows[i]);
+    print("${(i / (table.rows.length - 1)) * 100}");
+    data.getColData(table.rows[i]);
+  }
 
-    var a = await con.query(
-        " insert into CLASS_INFO (CLASS_NAME, CLASS_SECTOR_1, CLASS_SECTOR_TOTAL, CREDIT, CAMPUS, PROFESSOR, CLASS_TIME, HEAD_COUNT, CLASS_NUMBER, COLLEGE_NAME, COLLEGE_MAJOR, OPEN_TIME)" +
-            " values(?,?,?,?,?,?,?,?,?,?,?,?)",
-        [
-          data["CLASS_NAME"],
-          data["CLASS_SECTOR_1"],
-          data["CLASS_SECTOR_TOTAL"],
-          data["CREDIT"],
-          data["CAMPUS"],
-          data["PROFESSOR"],
-          json.encode(data["CLASS_TIME"]),
-          data["HEAD_COUNT"],
-          data["CLASS_NUMBER"],
-          data["COLLEGE_NAME"],
-          data["COLLEGE_MAJOR"],
-          [json.encode(data["OPEN_TIME"])]
-        ]);
+  var cst = File("CLASS_SECTOR_TOTAL.txt");
+  var cs1 = File("CLASS_SECTOR_1.txt");
+  var cn = File("COLLEGE_NAME.txt");
+  var cm = File("COLLEGE_MAJOR.txt");
 
-    log.writeAsString("${i} / ${table.rows.length - 1}\n",
+  await cst.writeAsString("CLASS_SECTOR_TOTAL\n");
+  await cs1.writeAsString("CLASS_SECTOR_1\n");
+  await cn.writeAsString("COLLEGE_NAME\n");
+  await cm.writeAsString("COLLEGE_MAJOR\n");
+
+  for (var i = 0; i < data.CLASS_SECTOR_TOTAL.length; i++) {
+    await cst.writeAsString("${data.CLASS_SECTOR_TOTAL[i]} ${i}\n",
         mode: FileMode.append);
   }
+
+  for (var i = 0; i < data.CLASS_SECTOR_1.length; i++) {
+    await cs1.writeAsString("${data.CLASS_SECTOR_1[i]} ${i}\n",
+        mode: FileMode.append, encoding: utf8);
+  }
+
+  for (var i = 0; i < data.COLLEGE_NAME.length; i++) {
+    await cn.writeAsString("${data.COLLEGE_NAME[i]} ${i}\n",
+        mode: FileMode.append, encoding: utf8);
+  }
+
+  for (var i = 0; i < data.COLLEGE_MAJOR.length; i++) {
+    await cm.writeAsString("${data.COLLEGE_MAJOR[i]} ${i}\n",
+        mode: FileMode.append, encoding: utf8);
+  }
+
+  print("${data.CLASS_SECTOR_TOTAL} :  ${data.CLASS_SECTOR_TOTAL.length}");
+  print("${data.CLASS_SECTOR_1} :  ${data.CLASS_SECTOR_1.length}");
+  print("${data.COLLEGE_NAME} :  ${data.COLLEGE_NAME.length}");
+  print("${data.COLLEGE_MAJOR} :  ${data.COLLEGE_MAJOR.length}");
+}
+
+void main(List<String> arguments) async {
+  // var file = "class.xlsx";
+  // var log_file = "log.txt";
+  // var error_file = "error.txt";
+
+  // var bytes = File(file).readAsBytesSync();
+  // var excel = Excel.decodeBytes(bytes);
+  // Sheet table = excel.tables["Sheet"];
+
+  MySqlConnection con = await getDB();
+
+  var cst_txt = File("CLASS_SECTOR_TOTAL.txt");
+  var cs1_txt = File("CLASS_SECTOR_1.txt");
+  var cn_txt = File("COLLEGE_NAME.txt");
+  var cm_txt = File("COLLEGE_MAJOR.txt");
+
+  var cst = await cst_txt.readAsLines();
+  var cs1 = await cs1_txt.readAsLines();
+  var cn = await cn_txt.readAsLines();
+  var cm = await cm_txt.readAsLines();
+
+  for (var item in cst) {
+    String key = item.split(" ")[0];
+    String value = item.split(" ")[1];
+    await con.query(
+        "UPDATE polarStar.CLASS_INFO SET INDEX_SECTOR_TOTAL = ? WHERE CLASS_SECTOR_TOTAL = ?",
+        [value, key]);
+  }
+
+  for (var item in cs1) {
+    if (item.isEmpty) {
+      break;
+    }
+    String key = item.split(" ").first;
+    String value = item.split(" ").last;
+    await con.query(
+        "UPDATE polarStar.CLASS_INFO SET INDEX_SECTOR_1 = ? WHERE CLASS_SECTOR_1 = ?",
+        [value, key]);
+  }
+
+  for (var item in cn) {
+    String key = item.split(" ")[0];
+    String value = item.split(" ")[1];
+    await con.query(
+        "UPDATE polarStar.CLASS_INFO SET INDEX_COLLEGE_NAME = ? WHERE COLLEGE_NAME = ?",
+        [value, key]);
+  }
+
+  for (var item in cm) {
+    String key = item.split(" ")[0];
+    String value = item.split(" ")[1];
+    await con.query(
+        "UPDATE polarStar.CLASS_INFO SET INDEX_COLLEGE_MAJOR = ? WHERE COLLEGE_MAJOR = ?",
+        [value, key]);
+  }
+  print("finish");
+  return;
 }
--- bin/read_exccel.dart
+++ bin/read_exccel.dart
@@ -70,3 +70,35 @@ Map<String, dynamic> getMapData(List<dynamic> rows) {
 
   return classModel.toJson();
 }
+
+class ColData {
+  List<String> CLASS_SECTOR_TOTAL = [];
+  List<String> CLASS_SECTOR_1 = [];
+  List<String> COLLEGE_NAME = [];
+  List<String> COLLEGE_MAJOR = [];
+
+  ColData();
+
+  void getColData(List<dynamic> rows) {
+    checkSectorTotal(rows[3], COLLEGE_NAME);
+    checkSectorTotal(rows[4], COLLEGE_MAJOR);
+
+    String CLASS_SECTOR = getClassSector(rows[6], rows[7]);
+    checkSectorTotal(CLASS_SECTOR, CLASS_SECTOR_1);
+    checkSectorTotal(rows[8], CLASS_SECTOR_TOTAL);
+  }
+
+  void checkSectorTotal(String item, List<String> CLASS_SECTOR_TOTAL) {
+    bool tag = true;
+    for (var i in CLASS_SECTOR_TOTAL) {
+      if (i == item) {
+        tag = false;
+        break;
+      }
+    }
+
+    if (tag) {
+      CLASS_SECTOR_TOTAL.add(item);
+    }
+  }
+}
